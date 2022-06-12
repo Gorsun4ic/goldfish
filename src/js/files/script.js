e8e933e54132e7290294112e3b8f4949b8db04c7
Gorsun4ic <gorsunfoster@ukr.net>
2022-06-12 10:39:48 +0300

    Багованное меню

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -1,4 +1,12 @@
 // Подключение функционала "Чертогов Фрилансера"
-import { isMobile } from "./functions.js";
+import {
+    isMobile
+} from "./functions.js";
 // Подключение списка активных модулей
-import { flsModules } from "./modules.js";
+import {
+    flsModules
+} from "./modules.js";
+let catalogBurger = document.querySelector(".catalog-burger");
+catalogBurger.onclick = function () {
+    document.querySelector(".header__categories").classList.toggle('header__categories_active');
+}
